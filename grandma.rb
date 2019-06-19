5b844786df1dde52e8b0269a5e04ece7c9036717
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-19 19:31:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,4 +3,6 @@ def speak_to_grandma
     return "I LOVE YOU TOO PUMPKIN"
   elsif "hi!"
     retun "HUH?! SPEAK UP, SONNY!"
+  elsif "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
     
